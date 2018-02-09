5d3e677006daf107cd7af7480f9d810bae23e3dd
Sean Miller <smiller206@me.com>
2018-02-09 14:43:34 -0800

    done with q1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
 
 // TODO: Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b) { 
   var theSum = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
   return [theSum, message];
@@ -22,6 +22,8 @@ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
+//Definitely skipping this part becuase the in the clone I forked from the repo, this was already working.
+
 /////////////////////////////////////
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments passed into the function:
@@ -31,12 +33,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // TODO: Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(a, b) { 
+  var mult = a * b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + mult + '.';
+  return [mult, message];
 }
 
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,9 +56,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // TODO: Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
-}
+// function sumAndMultiply(a, b, c) {
+//   var sumAB = sum(a,b);
+//   var sumABC = sum(sumAB,c);
+//   var multAB = multiply(a,b);
+//   var multABC = multiply(multAB,c);
+//   var msgSum = a + ' and ' + b + ' and ' + c + ' sum to ' + sumABC + '.';
+//   var msgMult = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multABC + '.';
+//   return [sumABC,multABC,msgSum,msgMult];
+// }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
